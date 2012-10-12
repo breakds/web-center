b659edf6dbc5d60fb4b5989b9117b1a67272ef17
BreakDS@gmail.com <breakds@gmail.com>
2012-10-11 20:09:13 -0500

    provide edzhong for bus

--- lisp/web-center.lisp
+++ lisp/web-center.lisp
@@ -28,7 +28,7 @@
 		 collect (list :stamp every))
 	      (list (list :stamp "Sorry, no bus is coming."))))))
 
-(hunchentoot:define-easy-handler (bus-handler :uri "/bus") ()
+(hunchentoot:define-easy-handler (bus-handler :uri "/bus") (user)
   (setf (hunchentoot:content-type*) "text/html")
   ;; (let ((output (make-string-output-stream)))N
   ;;   (format output "<u>Bus Info v0.2 for <b>breakds</b></u><br><br>~%")
@@ -36,14 +36,24 @@
   ;;   (loop for every in (bus-info:get-bus-info 2 115 886)
   ;;      do (format output "~a <br>" every))
   ;;   (get-output-stream-string output)))
-  (with-output-to-string (html-template:*default-template-output*)
-    (html-template:fill-and-print-template
-     (asdf:system-relative-pathname 'web-center "template/bus.template")
-     (list :stops 
-	   (list (create-bus-info "campus -> home, Route 2" 2 115 886)
-		 (create-bus-info "campus -> home, Route 28" 28 115 914)
-		 (create-bus-info "home -> campus, Route 2" 2 131 986)
-		 (create-bus-info "home -> campus, Route 28" 28 131 986))))))
+  (cond ((equal "edzhong" user)
+	 (with-output-to-string (html-template:*default-template-output*)
+	   (html-template:fill-and-print-template
+	    (asdf:system-relative-pathname 'web-center "template/bus.template")
+	    (list :stops 
+		  (list (create-bus-info "Mifflin & Pinckney, Route 6" 6 131 963)
+			(create-bus-info "Mifflin & Pinckney, Route 3" 3 131 963)
+			(create-bus-info "Mifflin & Pinckney, Route 7" 7 131 963)
+			(create-bus-info "Mifflin & Pinckney, Route 2" 2 131 963))))))
+	(t 
+	 (with-output-to-string (html-template:*default-template-output*)
+	   (html-template:fill-and-print-template
+	    (asdf:system-relative-pathname 'web-center "template/bus.template")
+	    (list :stops 
+		  (list (create-bus-info "campus -> home, Route 2" 2 115 886)
+			(create-bus-info "campus -> home, Route 28" 28 115 914)
+			(create-bus-info "home -> campus, Route 2" 2 131 986)
+			(create-bus-info "home -> campus, Route 28" 28 131 986))))))))
 
 	   
                                               
